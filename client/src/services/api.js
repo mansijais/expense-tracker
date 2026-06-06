9adc9ae6f2ad3005b5cef07788f291533a59a5c3
Mansi Jaiswal <mansigbu338@gmail.com>
2026-06-06 16:42:24 +0530

    Update backend production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:5009/api",
+    "https://expense-tracker-d0ji.onrender.com/api",
 });
 
 export default API;
